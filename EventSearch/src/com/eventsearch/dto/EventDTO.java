77ae9f282545174c11adb1a9d3252c842cd1565d
Akshay Jagtiani <ajagtian@usc.edu>
2014-11-23 07:35:19 -0800

    deleted source

--- EventSearch/src/com/eventsearch/dto/EventDTO.java
+++
@@ -1,5 +0,0 @@
-package com.eventsearch.dto;
-
-public class EventDTO {
-
-}
--- EventSearch/src/com/eventsearch/errors/ErrorCodes.java
+++
@@ -1,8 +0,0 @@
-package com.eventsearch.errors;
-
-
-public class ErrorCodes {
-
-	public static final String COULD_NOT_GET_APP_KEY = "app_1";
-	public static final String COULD_NOT_GET_APP_SECRET = "app_2";
-}
